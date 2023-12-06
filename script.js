6c468b5828202a2416eea8b17401c70d8bf74992
LiLMaxyy <dracher@list.ru>
2023-12-06 15:24:03 +0300

    Добавлена страница товара, новая логика магазина

--- script.js
+++ script.js
@@ -2,16 +2,27 @@
 
 const products = [
     
-    { name: 'Товар 1', price: 19.99, description: 'Описание товара 1', image: 'product1.jpg' },
-    { name: 'Товар 2', price: 29.99, description: 'Описание товара 2', image: 'product2.jpg' }
+    { id: 1, name: 'Товар 1', price: 19.99, category: 'Электроника', description: 'Описание товара 1', image: 'product1.jpg' },
+    { id: 2, name: 'Товар 2', price: 29.99, category: 'Одежда', description: 'Описание товара 2', image: 'product2.jpg' }
     
 ];
 
-let cartItems = [];
+let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
 
 function initializePage() {
     displayProducts(products);
     updateCart();
+    populateCategoryFilter();
+}
+
+function displayProducts(productsToDisplay) {
+    const productsSection = document.getElementById('productsSection');
+    productsSection.innerHTML = '';
+
+    productsToDisplay.forEach(product => {
+        const productElement = createProductElement(product);
+        productsSection.appendChild(productElement);
+    });
 }
 
 function updateCart() {
@@ -79,27 +90,67 @@ function createProductElement(product) {
     const productDescription = document.createElement('p');
     productDescription.textContent = product.description;
 
+    const productCategory = document.createElement('p');
+    productCategory.textContent = `Категория: ${product.category}`;
+
     const productPrice = document.createElement('p');
     productPrice.textContent = `Цена: $${product.price.toFixed(2)}`;
 
     const addToCartButton = document.createElement('button');
     addToCartButton.textContent = 'Добавить в корзину';
     addToCartButton.onclick = function () {
-        addToCart(product.name,addToCart(product.name, product.price);
+        addToCart(product.id);
     };
 
-    productDiv.appendChild(productName);
+    
+    const productLink = document.createElement('a');
+    productLink.href = `product.html?id=${product.id}`;
+    productLink.appendChild(productName);
+
+    productDiv.appendChild(productLink);
     productDiv.appendChild(productImage);
     productDiv.appendChild(productDescription);
+    productDiv.appendChild(productCategory);
     productDiv.appendChild(productPrice);
     productDiv.appendChild(addToCartButton);
 
     return productDiv;
 }
 
+function populateCategoryFilter() {
+    const categoryFilter = document.getElementById('categoryFilter');
+    const categories = [...new Set(products.map(product => product.category))];
+    
+    categories.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        categoryFilter.appendChild(option);
+    });
+}
+
 function clearCart() {
     cartItems = [];
     updateCart();
+}
+
+function filterProducts() {
+    const categoryFilter = document.getElementById('categoryFilter').value;
+    const searchQuery = document.getElementById('searchProduct').value.toLowerCase();
+
+    let filteredProducts = products;
+
+    if (categoryFilter !== 'all') {
+        filteredProducts = filteredProducts.filter(product => product.category === categoryFilter);
+    }
+
+    if (searchQuery) {
+        filteredProducts = filteredProducts.filter(product =>
+            product.name.toLowerCase().includes(searchQuery) || product.description.toLowerCase().includes(searchQuery)
+        );
+    }
+
+    displayProducts(filteredProducts);
 }
 
 window.onload = initializePage;
